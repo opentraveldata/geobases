f9df8407b6014249c084c32a02b449b2b759b0ab
Alex <alex.prengere@amadeus.com>
2012-07-19 11:47:22 +0200

    Unify errors, more consistant error messages, some refactoring, improve help messages

--- GeoBaseMain.py
+++ GeoBaseMain.py
@@ -70,8 +70,8 @@ class RotatingColors(object):
 
 def display(geob, list_of_things, omit, show, important):
 
-    if show is None:
-        show = geob._headers[:]
+    if not show:
+        show = ['__ref__'] + geob._headers[:]
 
     # Different behaviour given
     # number of results
@@ -88,12 +88,6 @@ def display(geob, list_of_things, omit, show, important):
         truncate = lim
 
     c = RotatingColors()
-    col = c.getHeader()
-
-    sys.stdout.write('\n' + fixed_width('ref', col, lim, truncate))
-
-    for (h, k) in list_of_things:
-        sys.stdout.write(fixed_width(h, col, lim, truncate))
 
     for f in show:
         if f not in omit:
@@ -103,10 +97,18 @@ def display(geob, list_of_things, omit, show, important):
             else:
                 col = c.get()
 
-            sys.stdout.write('\n' + fixed_width(f, col, lim, truncate))
+            if f == '__ref__':
 
-            for (h, k) in list_of_things:
-                sys.stdout.write(fixed_width(geob.get(k, f), col, lim, truncate))
+                sys.stdout.write('\n' + fixed_width(f, c.getHeader(), lim, truncate))
+
+                for h, k in list_of_things:
+                    sys.stdout.write(fixed_width('%.3f' % h, c.getHeader(), lim, truncate))
+
+            else:
+                sys.stdout.write('\n' + fixed_width(f, col, lim, truncate))
+
+                for _, k in list_of_things:
+                    sys.stdout.write(fixed_width(geob.get(k, f), col, lim, truncate))
 
             c.next()
 
@@ -115,7 +117,7 @@ def display(geob, list_of_things, omit, show, important):
 
 def display_quiet(geob, list_of_things, omit, show):
 
-    if show is None:
+    if not show:
         show = ['__ref__'] + geob._headers[:]
 
     for (h, k) in list_of_things:
@@ -126,7 +128,7 @@ def display_quiet(geob, list_of_things, omit, show):
             if f not in omit:
 
                 if f == '__ref__':
-                    l.append('%.5f' % h)
+                    l.append('%.3f' % h)
                 else:
                     l.append(geob.get(k, f))
 
@@ -141,41 +143,67 @@ def fixed_width(s, col, lim=25, truncate=None):
     return colored((('%-' + str(lim) + 's') % s)[0:truncate], *col)
 
 
-
 def scan_coords(u_input, geob, verbose):
 
     try:
-        coords = tuple(float(l) for l in u_input.split(','))
+        coords = tuple(float(l) for l in u_input.strip('()').split(','))
 
     except ValueError:
+        # Scan coordinates failed, perhaps input was key
+        if u_input not in geob:
+            error('key', u_input, geob._data, geob._source)
 
-        if u_input in geob:
-            return geob.getLocation(u_input)
-        else:
-            return None
+        return geob.getLocation(u_input)
 
     else:
-        if len(coords) == 2:
-            if verbose:
-                print '\nGeocode recognized: (%.3f, %.3f)' % coords
-            return coords
-        else:
-            if verbose:
-                print '\n/!\ Bad geocode format: %s' % u_input
-            return None
+        if len(coords) != 2:
+            error('geocode_format', u_input)
+
+        if verbose:
+            print 'Geocode recognized: (%.3f, %.3f)' % coords
+        return coords
 
 
-
-def display_on_two_cols(a_list):
+def display_on_two_cols(a_list, descriptor=sys.stdout):
     '''
     Some formatting for help.
     '''
 
     for p in zip(a_list[::2], a_list[1::2]):
-        print '\t%-15s\t%-15s' % p
+        print >> descriptor, '\t%-15s\t%-15s' % p
+
+    if len(a_list) % 2 != 0:
+        print >> descriptor, '\t%-15s' % a_list[-1]
 
 
 
+def error(name, *args):
+
+    if name == 'key':
+        print >> sys.stderr, '\n/!\ Key %s was not in GeoBase, for data "%s" and source %s' % \
+                (args[0], args[1], args[2])
+
+    elif name == 'geocode_support':
+        print >> sys.stderr, '\n/!\ No geocoding support for data type %s.' % args[0]
+
+    elif name == 'base':
+        print >> sys.stderr, '\n/!\ Wrong base "%s". You may select:' % args[0]
+        display_on_two_cols(args[1], sys.stderr)
+
+    elif name == 'property':
+        print >> sys.stderr, '\n/!\ Wrong property "%s".' % args[0]
+        print >> sys.stderr, 'For data type %s, you may select:' % args[1]
+        display_on_two_cols(args[2], sys.stderr)
+
+    elif name == 'field':
+        print >> sys.stderr, '\n/!\ Wrong field "%s".' % args[0]
+        print >> sys.stderr, 'For data type %s, you may select:' % args[1]
+        display_on_two_cols(args[2], sys.stderr)
+
+    elif name == 'geocode_format':
+        print >> sys.stderr, '\n/!\ Bad geocode format: %s' % args[0]
+
+    exit(1)
 
 
 def main():
@@ -205,7 +233,8 @@ def main():
 
     parser.add_argument('-f', '--fuzzy',
         help = '''Rather than looking up a key, this mode will search the best
-                        match from the property given by --property option.''',
+                        match from the property given by --property option. By default,
+                        the "name" property is used for the search.''',
         action='store_true'
     )
 
@@ -220,21 +249,23 @@ def main():
         help = '''Specify a limit when performing fuzzy searches, or geographical
                         searches. May be a radius in km or a number of results
                         given the context of the search (see --near, --closest and
-                       --fuzzy). Default is 3''',
+                       --fuzzy). Default is 3.''',
         default = 3
     )
 
     parser.add_argument('-n', '--near',
         help = '''Rather than looking up a key, this mode will search the entries
                         in a radius from a geocode or a key. Radius is given by --limit option,
-                        and geocode is passed as main argument.''',
+                        and geocode is passed as main argument. If you wish to give a geocode as
+                        input, just pass it as main argument with "lat, lng" format.''',
         action='store_true'
     )
 
     parser.add_argument('-c', '--closest',
         help = '''Rather than looking up a key, this mode will search the closest entries
                         from a geocode or a key. Number of results is limited by --limit option,
-                        and geocode is passed as main argument.''',
+                        and geocode is passed as main argument. If you wish to give a geocode as
+                        input, just pass it as main argument with "lat, lng" format.''',
         action='store_true'
     )
 
@@ -271,12 +302,12 @@ def main():
                         available keyword with the
                         other geobase headers.''',
         nargs = '+',
-        default = None
+        default = []
     )
 
     parser.add_argument('-u', '--update',
         help = '''If this option is set, before anything is done, 
-                        the script will try to update the oripor source file.''',
+                        the script will try to update the ori_por source file.''',
         action='store_true'
     )
 
@@ -295,12 +326,10 @@ def main():
     # FETCHING
     #
     if args['base'] not in GeoBase.BASES:
-        print '/!\ Wrong base "%s". You may select:' % args['base']
-        display_on_two_cols(list(GeoBase.BASES.keys()))
-        exit(1)
+        error('base', args['base'], list(GeoBase.BASES.keys()))
 
+    # Updating file
     if args['update']:
-        # Updating file
         GeoBase.update()
 
     if not args['quiet']:
@@ -328,50 +357,28 @@ def main():
     if args['fuzzy']:
 
         if args['property'] not in g._headers:
-
-            print '\n/!\ Wrong property "%s".' % args['property']
-            print 'For data type %s, you may select:' % args['base']
-            display_on_two_cols(list(g._headers))
-            exit(1)
+            error('property', args['property'], args['base'], list(g._headers))
 
         res = g.fuzzyGet(key, args['property'], approximate=int(limit))
 
-    elif args['near']:
+    elif args['near'] or args['closest']:
 
         if not g.hasGeoSupport():
-            print '\n/!\ No geocoding support for data type %s.' % args['base']
-            exit(1)
+            error('geocode_support', args['base'])
 
         coords = scan_coords(key, g, not(args['quiet']))
 
-        if coords is None:
-            print 'Key %s was not in GeoBase, for data "%s" and source %s' % \
-                (key, g._data, g._source)
-            exit(1)
+        if args['near']:
+            if args['without_grid']:
+                res = sorted(g.findNearPoint(*coords, radius=limit))
+            else:
+                res = sorted(g.gridFindNearPoint(*coords, radius=limit))
 
-        if args['without_grid']:
-            res = sorted(g.findNearPoint(*coords, radius=limit))
-        else:
-            res = sorted(g.gridFindNearPoint(*coords, radius=limit))
-
-
-    elif args['closest']:
-
-        if not g.hasGeoSupport():
-            print '\n/!\ No geocoding support for data type %s.' % args['base']
-            exit(1)
-
-        coords = scan_coords(key, g, not(args['quiet']))
-
-        if coords is None:
-            print 'Key %s was not in GeoBase, for data "%s" and source %s' % \
-                (key, g._data, g._source)
-            exit(1)
-
-        if args['without_grid']:
-            res = g.findClosestFromPoint(*coords, N=int(limit))
-        else:
-            res = g.gridFindClosestFromPoint(*coords, N=int(limit))
+        elif args['closest']:
+            if args['without_grid']:
+                res = g.findClosestFromPoint(*coords, N=int(limit))
+            else:
+                res = g.gridFindClosestFromPoint(*coords, N=int(limit))
 
     else:
         res = [(i, k) for i, k in enumerate(key)]
@@ -382,10 +389,13 @@ def main():
     #
     for (h, k) in res:
         if k not in g:
-            print 'Key %s was not in GeoBase, for data "%s" and source %s' % \
-                (k, g._data, g._source)
-            exit(1)
+            error('key', k, g._data, g._source)
 
+    for field in args['show'] + args['omit']: 
+
+        if field not in ['__ref__'] + list(g._headers):
+
+            error('field', field, args['base'], ['__ref__'] + list(g._headers))
 
     # Highlighting some rows
     important = set(['code', args['property']])
