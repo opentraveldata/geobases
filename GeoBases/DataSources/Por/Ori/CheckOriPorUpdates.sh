12b39323313d43ad24fae25235a96a44726990e3
Alex <alexprengere@gmail.com>
2012-07-24 10:15:58 +0200

    Clearer update mechanism

--- GeoBases/DataSources/Por/Ori/CheckOriPorUpdates.sh
+++ GeoBases/DataSources/Por/Ori/CheckOriPorUpdates.sh
@@ -9,7 +9,7 @@ TMP_CSV='tmp_01.csv'
 cd `dirname $0`
 LOC_CSV='ori_por_public.csv'
 
-echo "Considering local oripor file $LOC_CSV."
+echo -e "\nConsidering local oripor file $PWD/$LOC_CSV\n"
 
 
 # By default, we will ask the user permission to replace
@@ -30,27 +30,35 @@ while getopts ":f" opt; do
 done
 
 # Downloading
-wget $REF_URL -O $TMP_CSV
+wget $REF_URL -O $TMP_CSV # -o /dev/null
 
 # Commenting header
 sed -i '1s/^/#/g' $TMP_CSV
 
 echo "Unified diff:"
-diff -u $LOC_CSV $TMP_CSV
+DIFF=`diff -u $LOC_CSV $TMP_CSV`
+echo $DIFF
+echo
 
-if [ "$FORCE" == "0" ]; then
+if [ "$DIFF" = "" ]; then
+    echo "Nothing to do."
+    rm -f $TMP_CSV
+    exit 0
+fi
+
+if [ "$FORCE" = "0" ]; then
     echo -n "Replace? [Y/N]: "
     read RESPONSE
 else
     RESPONSE="Y"
 fi
 
-if [ "$RESPONSE" == "Y" ]; then
+if [ "$RESPONSE" = "Y" ]; then
     echo "You chose to replace."
     mv $TMP_CSV $LOC_CSV
 else
     echo "You chose not to replace."
-    rm $TMP_CSV
+    rm -f $TMP_CSV
 fi
 
 
