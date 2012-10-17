6503a06e2c15d55a18ec566cde5ce55434d64e00
Name <name@gmail.com>
2012-10-17 11:21:14 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'GeoBases',
-    version = '2.7.1',
+    version = '2.7.2',
     author = 'Alex Prengere',
     author_email = 'alex.prengere@amadeus.com',
     url = 'http://mediawiki.orinet.nce.amadeus.net/index.php/GeoBases',
