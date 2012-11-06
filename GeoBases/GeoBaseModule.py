d7aec9a118b28cd6ad51a6e42b05c13669d0bc53
Alex <alexprengere@gmail.com>
2012-11-06 12:56:50 +0100

    Fix tests, make it less sensitive to source changes.

--- GeoBases/GeoBaseModule.py
+++ GeoBases/GeoBaseModule.py
@@ -122,7 +122,7 @@ class GeoBase(object):
         Source was None, skipping loading...
         >>> geo_c = GeoBase(data='odd')
         Traceback (most recent call last):
-        ValueError: Wrong data type. Not in ['currencies', 'continents', 'timezones', 'ori_por_non_iata', 'location_types', 'stations_uic', 'stations_nls', 'airports_csv', 'ori_por', 'cities', 'locales', 'ori_por_multi', 'ori_por_private', 'airports', 'stations', 'countries', 'ori_por_private_multi', 'languages', 'regions', 'airlines']
+        ValueError: Wrong data type. Not in ['airlines', ...]
         >>> 
         >>> GeoBase(data='feed',
         ...         source=local_path(__file__, 'DataSources/Airports/AirportsDotCsv/ORI_Simple_Airports_Database_Table.csv'),
@@ -174,7 +174,7 @@ class GeoBase(object):
             pass
 
         else:
-            raise ValueError('Wrong data type. Not in %s' % GeoBase.BASES.keys())
+            raise ValueError('Wrong data type. Not in %s' % sorted(GeoBase.BASES.keys()))
 
         # Loading data
         self._loadFile()
