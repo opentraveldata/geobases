31f174a42f904886854d00019b40e4e1e11caf5b
Alex <alex.prengere@amadeus.com>
2012-11-27 18:03:29 +0100

    Bump minor version after airlines udpate

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ except ImportError:
 
 setup(
     name = 'GeoBases',
-    version = '4.1.1',
+    version = '4.1.2',
     author = 'Alex Prengere',
     author_email = 'alex.prengere@amadeus.com',
     url = 'http://mediawiki.orinet.nce.amadeus.net/index.php/GeoBases',
