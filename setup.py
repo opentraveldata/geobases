a3ce3ca523dc5b14ed473f4abe70f2b088b0b0ab
Alex <alex.prengere@amadeus.com>
2012-11-27 15:14:57 +0100

    Minor bump

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ except ImportError:
 
 setup(
     name = 'GeoBases',
-    version = '4.1.0',
+    version = '4.1.1',
     author = 'Alex Prengere',
     author_email = 'alex.prengere@amadeus.com',
     url = 'http://mediawiki.orinet.nce.amadeus.net/index.php/GeoBases',
