a5394d20b18482f504fe7ebbc8cfbd8c3bebfc59
Alex <alex.prengere@amadeus.com>
2012-12-04 11:40:41 +0100

    Use github as master reference for ori_por and ori_por_non_iata

--- GeoBases/DataSources/CheckDataUpdates.sh
+++ GeoBases/DataSources/CheckDataUpdates.sh
@@ -68,8 +68,10 @@ do_a_file() {
 }
 
 # Files
-REF_URL_1='http://redmine.orinet.nce.amadeus.net/projects/optd/repository/revisions/trunk/raw/refdata/ORI/ori_por_public.csv'
-REF_URL_2='http://redmine.orinet.nce.amadeus.net/projects/optd/repository/revisions/trunk/raw/refdata/ORI/ori_por_non_iata.csv'
+#REF_URL_1='http://redmine.orinet.nce.amadeus.net/projects/optd/repository/revisions/trunk/raw/refdata/ORI/ori_por_public.csv'
+#REF_URL_2='http://redmine.orinet.nce.amadeus.net/projects/optd/repository/revisions/trunk/raw/refdata/ORI/ori_por_non_iata.csv'
+REF_URL_1='https://github.com/opentraveldata/optd/raw/trunk/refdata/ORI/ori_por_public.csv'
+REF_URL_2='https://github.com/opentraveldata/optd/raw/trunk/refdata/ORI/ori_por_non_iata.csv'
 REF_URL_3='http://redmine.orinet.nce.amadeus.net/projects/oripor/repository/revisions/trunk/raw/admin/ori_por.csv'
 REF_URL_4='http://orinet.nce.amadeus.net/Projects/Data_Center/VOLATILE/airline/crb_airline.csv'
 
