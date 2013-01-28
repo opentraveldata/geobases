acbdc260b12de23bc36eebdcead6c3ac8a3b16e1
Alex <alexprengere@gmail.com>
2013-01-28 10:34:34 +0100

    Put OpenTravelData as project author

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GeoBases'
-copyright = u'2013, Alex Prengere'
+copyright = u'2013, OpenTravelData'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'GeoBases.tex', u'GeoBases Documentation',
-   u'Alex Prengere', 'manual'),
+   u'OpenTravelData', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'geobases', u'GeoBases Documentation',
-     [u'Alex Prengere'], 1)
+     [u'OpenTravelData'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'GeoBases', u'GeoBases Documentation',
-   u'Alex Prengere', 'GeoBases', 'One line description of project.',
+   u'OpenTravelData', 'GeoBases', 'One line description of project.',
    'Miscellaneous'),
 ]
 
