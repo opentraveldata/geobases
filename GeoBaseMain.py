82f910c2f394d4efea6ad03d3da58ea3c97d0692
Alex <alexprengere@gmail.com>
2013-01-31 12:30:47 +0100

    Changed any to and option, -a was confusing

--- GeoBaseMain.py
+++ GeoBaseMain.py
@@ -806,10 +806,10 @@ def handle_args():
         '''),
         action = 'store_true')
 
-    parser.add_argument('-a', '--any',
+    parser.add_argument('-a', '--and',
         help = dedent('''\
-        By default, --exact multiple searches are combined with *and*,
-        passing this option will change that to a *or*.
+        By default, --exact multiple searches are combined with *or*,
+        passing this option will change that to a *and*.
         '''),
         action = 'store_true')
 
@@ -1332,7 +1332,7 @@ def main():
 
         exact_values = args['exact'].split(SPLIT, len(exact_properties) - 1)
         conditions = list(izip_longest(exact_properties, exact_values, fillvalue=''))
-        mode = 'or' if args['any'] else 'and'
+        mode = 'and' if args['and'] else 'or'
 
         if verbose:
             if args['reverse']:
