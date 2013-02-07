246535b1d061f6f3d0b8f6cf18671700f66064da
Alex <alexprengere@gmail.com>
2013-02-07 19:07:58 +0100

    Tweak coverage

--- coverage/generate.sh
+++ coverage/generate.sh
@@ -8,12 +8,11 @@ cd ..
 pip uninstall GeoBases
 export PYTHONPATH=.:..:$PYTHONPATH
 
-echo -e "Running..."
 coverage erase
 coverage run ./test/test_GeoBases.py -v
 rm -f $DIRNAME/htmlcov/*
 coverage html --directory=$DIRNAME/htmlcov --omit="$HOME/.local*"
 
 # Results
-echo -e "\n\n* HTML results in $DIRNAME/htmlcov"
+echo -e "\n* HTML results in $DIRNAME/htmlcov"
 firefox $DIRNAME/htmlcov/index.html
