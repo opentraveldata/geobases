6ef5339ee280329218e30bafc0bb9694dc781e42
Alex <alexprengere@gmail.com>
2013-02-18 17:26:38 +0100

    Move logging in initialize

--- GeoBases/GraphAssets/graph.js
+++ GeoBases/GraphAssets/graph.js
@@ -27,16 +27,6 @@ function overflow(text) {
 }
 
 
-var Log = {
-    elem: false,
-    write: function(text){
-        if (!this.elem) {
-            this.elem = document.getElementById('log');
-        }
-        this.elem.innerHTML = text;
-        this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
-    }
-};
 
 
 function initialize(jsonData) {
@@ -257,6 +247,17 @@ function initialize(jsonData) {
 
     // load JSON data.
     fd.loadJSON(data);
+
+    var Log = {
+        elem: false,
+        write: function(text){
+            if (!this.elem) {
+                this.elem = document.getElementById('log');
+            }
+            this.elem.innerHTML = text;
+            this.elem.style.left = (500 - this.elem.offsetWidth / 2) + 'px';
+        }
+    };
 
     // compute positions incrementally and animate.
     fd.computeIncremental({
