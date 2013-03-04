1f781d0bfd1a7457e49aa91294214ccb104cee12
Alex <alexprengere@gmail.com>
2013-03-04 15:27:34 +0100

    Update comments

--- GeoBases/GeoBaseModule.py
+++ GeoBases/GeoBaseModule.py
@@ -635,7 +635,7 @@ class GeoBase(object):
             except KeyError:
                 # Here we have some fields that failed
                 # This can happen if incomplete key information
-                # has been supplied after loading, with set or setFromDict
+                # has been supplied after loading
                 if verbose:
                     print '/!\ Could not compute values for key "%s" and fields %s' % \
                             (key, str(fields))
