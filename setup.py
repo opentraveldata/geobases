c364c039f127e90e57d0a2d5b312a399f87ed9f9
Alex <alexprengere@gmail.com>
2016-06-13 03:51:47 -0400

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ EXTRAS_REQUIRE = {
     'OpenTrep': ['OpenTrepWrapper>=0.6']
 }
 
-DEPENDENCY_LINKS        = []
+DEPENDENCY_LINKS = []
 DEPENDENCY_LINKS_EXTRAS = {
     'OpenTrep' : ['https://github.com/trep/wrapper/tarball/master#egg=OpenTrepWrapper-0.7']
 }
@@ -45,16 +45,16 @@ if WITH_OPENTREP == '1':
     DEPENDENCY_LINKS.extend(DEPENDENCY_LINKS_EXTRAS['OpenTrep'])
 
     print >> stderr, '/!\\ Adding "%s" to mandatory dependencies' % \
-            str(EXTRAS_REQUIRE['OpenTrep'])
+        str(EXTRAS_REQUIRE['OpenTrep'])
 else:
     print >> stderr, '/!\\ Installing without "%s"' % \
-            str(EXTRAS_REQUIRE['OpenTrep'])
+        str(EXTRAS_REQUIRE['OpenTrep'])
 
 
 try:
     # monkey patch: Crack SandboxViolation verification from
     # http://www.cubicweb.org/1422923
-    #import setuptools.command.easy_install # only if easy_install avaible
+    # import setuptools.command.easy_install # only if easy_install avaible
     from setuptools.sandbox import DirectorySandbox as DS
 
     def _ok(*_):
@@ -67,51 +67,44 @@ except ImportError:
     raise
 
 
-# local files handling
-def local(rel_path, root_file=__file__):
-    """Handle local paths.
-    """
-    return op.join(op.realpath(op.dirname(root_file)), rel_path)
-
-with open(local('VERSION')) as fl:
+with open('VERSION') as fl:
     VERSION = fl.read().rstrip()
 
-with open(local('README.rst')) as fl:
+with open('README.rst') as fl:
     LONG_DESCRIPTION = fl.read()
 
-with open(local('LICENSE')) as fl:
+with open('LICENSE') as fl:
     LICENSE = fl.read()
 
 setup(
-    name = 'GeoBasesDev',
-    version = VERSION,
-    author = 'Alex Prengère',
-    author_email = 'geobases.dev@gmail.com',
-    url = 'http://opentraveldata.github.com/geobases',
-    description = 'Data services and visualization - development version',
-    long_description = LONG_DESCRIPTION,
-    license = LICENSE,
+    name='GeoBasesDev',
+    version=VERSION,
+    author='Alex Prengère',
+    author_email='geobases.dev@gmail.com',
+    url='http://opentraveldata.github.com/geobases',
+    description='Data services and visualization - development version',
+    long_description=LONG_DESCRIPTION,
+    license=LICENSE,
     #
     # Manage standalone scripts
-    entry_points = {
+    entry_points={
         'console_scripts' : [
             'GeoBase = GeoBaseMain:main'
         ]
     },
-    #packages=find_packages(),
-    packages = [
+    packages=[
         'GeoBases'
     ],
-    py_modules = [
+    py_modules=[
         'GeoBaseMain'
     ],
-    dependency_links = DEPENDENCY_LINKS,
-    install_requires = INSTALL_REQUIRES,
-    extras_require   = EXTRAS_REQUIRE,
-    package_dir = {
+    dependency_links=DEPENDENCY_LINKS,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    package_dir={
         'GeoBases': 'GeoBases'
     },
-    package_data = {
+    package_data={
         'GeoBases': [
             "DataSources/Sources.yaml",
             "DataSources/CheckDataUpdates.sh",
@@ -129,14 +122,7 @@ setup(
             "DataSources/*/*/*.zip",
         ]
     },
-    #scripts = [
-    #    'GeoBases/DataSources/CheckDataUpdates.sh'
-    #],
-    data_files = [
-        # Tests should not be exported
-        #('test', [
-        #    'test/test_GeoBases.py'
-        #]),
+    data_files=[
         # Will create dir if needed
         (op.join(getenv('HOME', '.'), '.zsh/completion'), [
             'GeoBases/completion/_GeoBase'
@@ -166,4 +152,3 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
-
